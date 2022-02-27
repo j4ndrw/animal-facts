0af3c37e15dd4c7522d985d462af952f73b90e1f
j4ndrw <murjan_andreialin@yahoo.com>
2022-02-27 15:01:42 +0200

    MFE approach with Vite + manifest.json | Success (locally)!

--- mfe/app/src/App.tsx
+++ mfe/app/src/App.tsx
@@ -2,6 +2,10 @@ import MFE from "./MFE";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+const catHost = process.env.CAT_HOST || "http://localhost:3001";
+const dogHost = process.env.DOG_HOST || "http://localhost:3002";
+const squirrelHost = process.env.SQUIRREL_HOST || "http://localhost:3003";
+
 function App() {
     return (
         <Router>
@@ -9,17 +13,15 @@ function App() {
                 <Route path="/" element={<h1>Main App</h1>} />
                 <Route
                     path="/cat"
-                    element={<MFE name="Cat" host="http://localhost:3001" />}
+                    element={<MFE name="Cat" host={catHost} />}
                 />
                 <Route
                     path="/dog"
-                    element={<MFE name="Dog" host="http://localhost:3002" />}
+                    element={<MFE name="Dog" host={dogHost} />}
                 />
                 <Route
                     path="/squirrel"
-                    element={
-                        <MFE name="Squirrel" host="http://localhost:3003" />
-                    }
+                    element={<MFE name="Squirrel" host={squirrelHost} />}
                 />
             </Routes>
         </Router>
